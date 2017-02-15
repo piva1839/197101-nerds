476eb507f020af4c4c44f7b9e252aad6ff12905e
Oleksii Oboianskyi <piva1839@gmail.com>
2017-02-15 17:08:10 +0200

    modal map в процессе
    modal map в процессе

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 var writeToUsBtn = document.querySelector(".btn-adress");
 var modalContactBlock = document.querySelector(".modal-contact-block");
 var closeModalBlock = document.querySelector(".close-modal-block");
-var popupOverlay = document.querySelector(".popup-overlay");
 var map = document.querySelector(".map");
+var modalMap = document.querySelector(".modal-map");
+var closeModalMap - document.querySelector(".close-modal-map");
+var popupOverlay = document.querySelector(".popup-overlay");
 
-writeToUsBtn.addEventListener("click", function(event) {
-	event.preventDefault();
-	modalContactBlock.classList.add("modal-contact-block-open");
-	popupOverlay.classList.add("popup-overlay-open");
-});
+	writeToUsBtn.addEventListener("click", function(event) {
+		event.preventDefault();
+		modalContactBlock.classList.add("modal-contact-block-open");
+		popupOverlay.classList.add("popup-overlay-open");
+	});
+	
+	closeModalBlock.addEventListener("click", function(event) {
+		event.preventDefault();
+		modalContactBlock.classList.remove("modal-contact-block-open");
+		popupOverlay.classList.remove("popup-overlay-open");
+	});
+	
+	
