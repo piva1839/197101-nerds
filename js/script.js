dfc694bc8dc8535591e60443a3e0e24a3de476b0
Oleksii Oboianskyi <piva1839@gmail.com>
2017-02-15 17:47:32 +0200

    js в процессе
    js в процессе

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var modalContactBlock = document.querySelector(".modal-contact-block");
 var closeModalBlock = document.querySelector(".close-modal-block");
 var map = document.querySelector(".map");
 var modalMap = document.querySelector(".modal-map");
-var closeModalMap - document.querySelector(".close-modal-map");
+var closeModalMap = document.querySelector(".close-modal-map");
 var popupOverlay = document.querySelector(".popup-overlay");
 
 	writeToUsBtn.addEventListener("click", function(event) {
@@ -18,4 +18,14 @@ var popupOverlay = document.querySelector(".popup-overlay");
 		popupOverlay.classList.remove("popup-overlay-open");
 	});
 	
+	map.addEventListener("click", function(event) {
+		event.preventDefault();
+		modalMap.classList.add("modal-map-open");
+		popupOverlay.classList.add("popup-overlay-open");
+	});
 	
+	closeModalMap.addEventListener("click", function(event) {
+		event.preventDefault();
+		modalMap.classList.remove("modal-map-open");
+		popupOverlay.classList.remove("popup-overlay-open");
+	});
